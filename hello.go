53b76691a67757119e962d02942ce50f50639c49
asalmu16 <asalmu16@student.uia.no>
2017-05-11 14:34:36 +0200

    Filer fra ICA01's gamle repo

---
+++ hello.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+import "math"
+
+
+func main() {
+	fmt.Println("Hello, ??")
+	fmt.Println("Heisann")
+	fmt.Println("Hahaha Adrian!?")
+        fmt.Println("Bastian sier hei")
+	log2()
+	fmt.Println("heisann paa deisann")
+	var gitBash string = "Git Bash"
+	or := " or die trying"
+	fmt.Println(gitBash, or)
+}
+
+func log2() {
+fmt.Println(math.Log2(2))
+}
---
+++ olgLog/log/log.go
@@ -0,0 +1,21 @@
+package log
+
+import "math"
+
+
+func Log2(x float64) float64  {
+
+answer := math.Log2(x)
+//var intAnswer int = int(answer)
+
+return answer
+
+}
+
+func Log10(x float64) float64 {
+
+  answer := math.Log10(x)
+  //var intAnswer int = int(answer)
+
+  return answer
+}
